6fda26cbdb6ab064c2788761f3dc9dafe222daea
matheussouza89 <matheus.souza89@fatec.sp.gov.br>
2020-11-09 16:50:24 -0300

    Parametro de sorteios(6,8,9,10,12,15) /Adição de Dummy

--- lib/algorithms/sorteio.dart
+++ lib/algorithms/sorteio.dart
@@ -3,6 +3,11 @@ import 'package:randomfut/components/player_tile.dart';
 
 var a = vetJogador;
 Random r = Random();
+var timeA = [];
+var timeB = [];
+var timeC = [];
+var timeD = [];
+var timeE = [];
 
 sorteio() {
   var sorteados = [];
@@ -13,5 +18,42 @@ sorteio() {
     var aux = r.nextInt(a.length);
     sorteados[aux] == '' ? sorteados[aux] = a[i] : i--;
   }
+  if (sorteados.length == 10 ||
+      sorteados.length == 8 ||
+      sorteados.length == 6) {
+    // Esta condição é para o caso de 2 Times
+    timeA = [];
+    timeB = [];
+    for (int i = 0; i < sorteados.length / 2; i++) {
+      timeA.add('');
+      timeB.add('');
+    }
+    for (int i = 0; i < sorteados.length / 2; i++) {
+      timeA[i] = sorteados[i];
+      timeB[i] = sorteados[((sorteados.length - 1) - i)];
+    }
+  }
+  if (sorteados.length == 15 ||
+      sorteados.length == 12 ||
+      sorteados.length == 9) {
+    // Esta condição é para o caso de 3 Times
+    timeA = [];
+    timeB = [];
+    timeC = [];
+    for (int i = 0; i < sorteados.length / 3; i++) {
+      timeA.add('');
+      timeB.add('');
+      timeC.add('');
+    }
+    for (int i = 0; i < sorteados.length / 3; i++) {
+      timeA[i] = sorteados[i];
+      timeB[i] =
+          sorteados[((sorteados.length - 1) - (sorteados.length ~/ 3)) - i];
+      timeC[i] = sorteados[((sorteados.length - 1) - i)];
+    }
+  }
   print(sorteados);
+  print(timeA);
+  print(timeB);
+  print(timeC);
 }
